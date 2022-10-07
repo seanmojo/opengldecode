98633d7bdc76c31fdff5cff16a3958f60b3c0907
Sean Ashmore <sean@mojo.video>
2022-10-07 10:25:21 +0100

    Add ability to choose a video

--- app/src/main/java/com/example/opengldecode/MainActivity.kt
+++ app/src/main/java/com/example/opengldecode/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.opengldecode
 
 import android.opengl.GLSurfaceView
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var glSurfaceView: GLSurfaceView
 
+    private val pickMedia =
+        registerForActivityResult(PickVisualMedia()) { uri ->
+            Log.i("MOJO", "Got uri: $uri")
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         glSurfaceView = MojoSurfaceView(this)
         setContentView(glSurfaceView)
+
+        pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.VideoOnly))
     }
 }
