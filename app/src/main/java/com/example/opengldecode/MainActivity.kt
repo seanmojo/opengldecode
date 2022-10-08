a34132b3de331ec9abbfa59501dcde4778f95a53
Sean Ashmore <sean@mojo.video>
2022-10-08 15:21:04 +0100

    Re-working the layout a bit

--- app/src/main/java/com/example/opengldecode/MainActivity.kt
+++ app/src/main/java/com/example/opengldecode/MainActivity.kt
@@ -1,29 +1,38 @@
 package com.example.opengldecode
 
-import android.opengl.GLSurfaceView
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.FrameLayout
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var glSurfaceView: GLSurfaceView
+    lateinit var mojoSurfaceView: MojoSurfaceView
+    lateinit var content: FrameLayout
 
     private val pickMedia =
         registerForActivityResult(PickVisualMedia()) { uri ->
             Log.i("MOJO", "Got uri: $uri")
 
             uri?.let {
-                glSurfaceView = MojoSurfaceView(this, it)
-                setContentView(glSurfaceView)
+                onMediaPicked(uri)
             }
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        content = findViewById(R.id.content)
 
         pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.VideoOnly))
+    }
+
+    private fun onMediaPicked(uri: Uri) {
+        mojoSurfaceView = MojoSurfaceView(this, uri)
+        content.addView(mojoSurfaceView)
     }
 }
